4e4b22dcc8c3fbfbef96ef836fe832ada9734795
Melissa Gonzales <melissa.gonzales@sky.uk>
2022-08-18 13:19:12 +0100

    exercise 6

---
+++ exercise6.py
@@ -0,0 +1,14 @@
+import sys 
+
+def calculate_fibonacci(to_position):
+    current_position = 2
+    fib_numbers = [1,2]
+    if to_position < 2:
+        return fib_numbers[to_position-1]
+    while current_position < to_position:
+        fib_numbers.append(fib_numbers[current_position-2]+fib_numbers[current_position-1])
+        current_position += 1
+    return fib_numbers[to_position-1]
+        
+to_pos = int(sys.argv[1])
+print(calculate_fibonacci(to_pos))
