c23bfc7c8071c84dc3e889ff3d4fe92a51c3c73f
Melissa Gonzales <melissa.gonzales@sky.uk>
2022-08-18 11:56:22 +0100

    exercise 2 and exercise 3

--- hello.py
+++ hello.py
@@ -1,5 +1,2 @@
-name = input("insert your name: ")
-age = int(input("insert your age: "))
-birth_year = 2022-age
-
-print(f"{name}, you were {1999-birth_year} years old when the London Eye opened in 1999")
+msg = "Hello World"
+print(msg)
